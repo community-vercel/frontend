0aba3c8e495ce9ab77fa198c6a34ca9fe3c509f9
community-vercel <communityvercel@gmail.com>
2025-08-24 22:09:13 +0500

    aa

--- app/layout.js
+++ app/layout.js
@@ -11,12 +11,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <title>EOS Issue Tracking & Task Management</title>
-
       </head>
       <body className="font-inter antialiased bg-gradient-to-br from-gray-50 via-blue-50 to-indigo-200 min-h-screen">
         <div className="flex min-h-screen">
           <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
-
           <div className="flex-1 flex flex-col">
             <Header toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
             <main className="flex-1 p-4 sm:p-6 lg:p-8">
